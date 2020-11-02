b6df3b66c22cc1f64a34910e519c7f34d0b030d7
Dabod <72407383+Dabod@users.noreply.github.com>
2020-11-02 22:52:11 +0100

    Add files via upload

---
+++ Coldware/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int opcionMenu = -1;
+
+		Scanner teclado = new Scanner(System.in);
+		Partida partida = new Partida();
+
+		System.out.println("Bienvenido/a a");
+		System.out.println(" ___   _____ _____ _    ______   _    _  ___  ______   ___ ");
+		System.out.println("|  _| /  __ \\  _  | |   |  _  \\ | |  | |/ _ \\ | ___ \\ |_  |");
+		System.out.println("| |   | /  \\/ | | | |   | | | | | |  | / /_\\ \\| |_/ /   | |");
+		System.out.println("| |   | |   | | | | |   | | | | | |/\\| |  _  ||    /    | |");
+		System.out.println("| |   | \\__/\\ \\_/ / |___| |/ /  \\  /\\  / | | || |\\ \\    | |");
+		System.out.println("| |_   \\____/\\___/\\_____/___/    \\/  \\/\\_| |_/\\_| \\_|  _| |");
+		System.out.println("|___|                                                 |___|");
+		System.out.println("                                                           ");
+		partida.parar(3000);
+
+		while (opcionMenu != 0) {
+			System.out.println("\n1 - Jugar\n");
+			System.out.println("2 - Reglas del Juego\n");
+			System.out.println("3 - Información\n");
+			System.out.println("4 - Opciones Musica\n");
+			System.out.println("0 - Salir");
+			System.out.println("\nElige una opción del menú introduciendo el número correspondiente:\n");
+			opcionMenu = teclado.nextInt();
+			while (opcionMenu < 0 || opcionMenu > 4) {
+				System.err.println(
+						"!Opción invalida! Asegurate de escoger un número correspondiente a alguna opción del menú.");
+
+				System.out.println("\n1 - Jugar\n");
+				System.out.println("2 - Reglas del Juego\n");
+				System.out.println("3 - Información\n");
+				System.out.println("4 - Opciones Musica\n");
+				System.out.println("0 - Salir");
+				System.out.println("\nElige una opción del menú introduciendo el número correspondiente:\n");
+				opcionMenu = teclado.nextInt();
+
+			}
+			switch (opcionMenu) {
+			case 1:
+				partida.iniciarPartida();
+				break;
+			case 2:
+				partida.reglasJuego();
+				break;
+			case 3:
+				partida.infoJuego();
+				break;
+			case 4:
+				partida.opcionesReproductor();
+				System.out.println("WIP");
+				break;
+			case 0:
+				System.out.println("¡¡¡ADIÓS MUY BUENAS!!!");
+				break;
+			}
+		}
+		System.out.println("Fin del programa.");
+		teclado.close();
+	}
+}
---
+++ Coldware/Planeta.java
@@ -0,0 +1,87 @@
+
+public class Planeta {
+
+	// Atributs
+	private int numEquipos; //No se usa de momento
+	private String nombreEquipo;
+	private int vidas;
+	private int misilesRonda;
+	private int misilesDefensa;
+	private boolean vivo;
+
+	public Planeta() {
+
+		this.vidas = 200;
+		this.misilesRonda = 50;
+		this.numEquipos++;
+	}
+
+	public int calcularDmg(int misiles) {
+		if (misiles >= misilesDefensa) {
+			this.vidas = this.vidas + misilesDefensa - misiles;
+		} else {
+			return this.vidas;
+		}
+		if (this.vidas < 0) {
+			this.vidas = 0;
+		}
+		return this.vidas;
+	}
+
+	// GETTERS Y SETTERS
+
+	// VIDAS
+	public int getVidas() {
+		return vidas;
+	}
+
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+
+	// PROPIEDAD VIVO
+	public boolean isVivo() {
+		return vivo;
+	}
+
+	public void setVivo(boolean vivo) {
+		this.vivo = vivo;
+	}
+
+	// NOMBRE EQUIPO
+	public String getNombreEquipo() {
+		return nombreEquipo;
+	}
+
+	public void setNombreEquipo(String nombreEquipo) {
+		this.nombreEquipo = nombreEquipo;
+	}
+
+	// NUMERO DE EQUIPOS
+	public int getNumEquipos() {
+		return numEquipos;
+	}
+
+	public void setNumEquipos(int numEquipos) {
+		this.numEquipos = numEquipos;
+	}
+
+	// MISILES DISPONIBLESd
+	public int getMisilesRonda() {
+		return misilesRonda;
+	}
+
+	public void setMisilesRonda(int misilesRonda) {
+		this.misilesRonda = misilesRonda;
+	}
+
+	// MISILES DE DEFENSA DE LA RONDA
+	public int getMisilesDefensa() {
+		return misilesDefensa;
+	}
+
+	public void setMisilesDefensa(int misilesDefensa) {
+		this.misilesDefensa = misilesDefensa;
+	}
+
+}
